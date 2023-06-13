fd5b552296759b49d23045c4c616f23932a8afd6
Rishiv Yadav <rishavyadav51@gmail.com>
2023-06-13 16:57:43 +0530

    expreess

--- src/main.js
+++ src/main.js
@@ -1,18 +1,9 @@
-import { insertMessage, readMessage } from "./mydb.js";
+// remotely executable
+// Network call
+// Network Address
+// Remote address
+// http://localhost:4000/
+import express from "express";
+const app = express();
 
-async function main() {
-  // insert
-  let jsonDocument = {
-    message: "Hello Universee",
-    to: "santosh",
-    from: "tejas",
-  };
-  await insertMessage(jsonDocument);
-
-  // read
-  let list = await readMessage();
-  console.log(list);
-}
-
-main();
-  
+app.listen(4000);
